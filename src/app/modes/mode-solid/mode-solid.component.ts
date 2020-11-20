9b9863a3381f2892c8ca0c48a91939ce531fc419
Xels <00deansen@gmx.de>
2020-11-20 13:23:50 +0100

    solid paramter sync

--- src/app/modes/mode-solid/mode-solid.component.ts
+++ src/app/modes/mode-solid/mode-solid.component.ts
@@ -3,10 +3,13 @@ import { RGB } from '../../color/color';
 import { REST_MODE_SOLID_URL } from '../../config/const';
 import { UpdateService } from '../../update/update.service';
 import { HttpClient } from '@angular/common/http';
+import { deepCopy } from '../../lib/deep-copy';
+import { deepEqual } from 'fast-equals';
 
 
 interface ModeSolidParameter {
   rgb: RGB,
+  brightness: number,
 }
 
 @Component({
@@ -16,27 +19,34 @@ interface ModeSolidParameter {
 })
 export class ModeSolidComponent implements OnInit {
   public modeSolidParameter: ModeSolidParameter
+  public uiModeSolidParameter: ModeSolidParameter
 
-  constructor(private httpClient: HttpClient, private updateService:UpdateService) {
+  constructor(private httpClient: HttpClient, private updateService: UpdateService) {
     this.modeSolidParameter = {
-      rgb: { r: 0, g: 0, b: 0 }
+      rgb: { r: 0, g: 0, b: 0 },
+      brightness: 1.0
     }
+    this.uiModeSolidParameter = this.modeSolidParameter
   }
 
   ngOnInit(): void {
-    console.log("this")
     this.updateModeSolidParameter();
-    this.updateService.registerPolling({ cb: ()=>{this.updateModeSolidParameter()}, timeout: 500 })
+    this.updateService.registerPolling({ cb: () => { this.updateModeSolidParameter() }, timeout: 500 })
   }
 
   updateModeSolidParameter() {
     this.httpClient.get<ModeSolidParameter>(REST_MODE_SOLID_URL).subscribe((data: ModeSolidParameter) => {
-      this.modeSolidParameter = data
-    });
+      if (!deepEqual(this.modeSolidParameter, data)) {
+        this.modeSolidParameter = data
+        this.uiModeSolidParameter = deepCopy(this.modeSolidParameter)
+      }
+    }
+    )
   }
 
   setModeSolidParameter() {
-    this.httpClient.post<ModeSolidParameter>(REST_MODE_SOLID_URL,this.modeSolidParameter,{}).subscribe()
+    console.log("set")
+    this.httpClient.post<ModeSolidParameter>(REST_MODE_SOLID_URL, this.uiModeSolidParameter, {}).subscribe()
   }
 
 }
