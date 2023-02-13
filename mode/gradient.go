133d10b758078f4f51f5393564c0b72617858be9
xels21 <dean.poslovsky@gmail.com>
2023-02-13 20:57:49 +0100

    gradient v2

--- mode/gradient.go
+++ mode/gradient.go
@@ -9,36 +9,48 @@ import (
 	"time"
 )
 
+type ModeGradientPosition struct {
+	hue_from     float64
+	hue_to       float64
+	hue_current  float64
+	hue_distance float64
+	positive     bool
+	roundTimeMs  uint32
+	percentStep  float64
+	percent      float64
+	limits       *ModeGradientLimits
+}
+
 type ModeGradient struct {
 	ModeSuper
-	parameter           ModeGradientParameter
-	limits              ModeGradientLimits
-	ledsHSV             []color.HSV
-	hues                [3]float64
-	clockwise           [3]bool
-	progressDegStepSize float64
-	progressDegStep     float64
+	parameter ModeGradientParameter
+	limits    ModeGradientLimits
+	ledsHSV   []color.HSV
+	positions []ModeGradientPosition
 }
 
 type ModeGradientParameter struct {
-	Brightness  float64 `json:"brightness"`
-	RoundTimeMs uint32  `json:"roundTimeMs"`
-	Reverse     bool    `json:"reverse"`
+	Brightness float64 `json:"brightness"`
+	Count      uint32  `json:"count"`
 }
 type ModeGradientLimits struct {
 	MinRoundTimeMs uint32  `json:"minRoundTimeMs"`
 	MaxRoundTimeMs uint32  `json:"maxRoundTimeMs"`
 	MinBrightness  float64 `json:"minBrightness"`
 	MaxBrightness  float64 `json:"maxBrightness"`
+	MinCount       uint32  `json:"minCount"`
+	MaxCount       uint32  `json:"maxCount"`
 }
 
 func NewModeGradient(dbdriver *dbdriver.DbDriver, display *display.Display) *ModeGradient {
 	self := ModeGradient{
 		limits: ModeGradientLimits{
-			MinRoundTimeMs: 500,   //500ms
-			MaxRoundTimeMs: 30000, //30sek
+			MinRoundTimeMs: 2000,
+			MaxRoundTimeMs: 10000,
 			MinBrightness:  0.1,
 			MaxBrightness:  1.0,
+			MinCount:       2,
+			MaxCount:       10,
 		},
 	}
 
@@ -48,12 +60,6 @@ func NewModeGradient(dbdriver *dbdriver.DbDriver, display *display.Display) *Mod
 	for i := 0; i < len(self.ledsHSV); i++ {
 		self.ledsHSV[i] = color.HSV{H: 0.0, S: 1.0, V: 0.0}
 	}
-
-	for i := 0; i < 3; i++ {
-		self.shiftGradiant()
-	}
-
-	self.progressDegStep = rand.Float64() * 360.0
 
 	err := dbdriver.Read(self.GetName(), "parameter", &self.parameter)
 	if err != nil {
@@ -86,54 +92,95 @@ func (self *ModeGradient) setParameter(parm ModeGradientParameter) {
 	self.postSetParameter()
 }
 
+func (self *ModeGradientPosition) StepForward() {
+	self.hue_current = self.hue_from + self.hue_distance*self.percent
+	if self.hue_current < 0 {
+		self.hue_current += 360
+	} else if self.hue_current > 360 {
+		self.hue_current -= 360
+	}
+
+	self.percent += self.percentStep
+	if self.percent > 100 {
+		self.percent -= 100
+		self.hue_from = self.hue_to
+		self.Randomize_wo_from()
+	}
+}
+func (self *ModeGradientPosition) Randomize_wo_from() {
+	self.hue_to = rand.Float64() * 360.0
+	self.positive = rand.Uint32()&1 == 1
+	self.roundTimeMs = uint32(rand.Float32()*float32(self.limits.MaxRoundTimeMs-self.limits.MinRoundTimeMs)) + self.limits.MinRoundTimeMs
+	self.percentStep = 100 / (float64(self.roundTimeMs) / 1000) * (float64(RefreshIntervalNs) / 1000 / 1000 / 1000)
+
+	self.hue_distance = self.hue_to - self.hue_from
+	if self.hue_distance < 0 && self.positive {
+		self.hue_distance += 360.0
+	} else if self.hue_distance > 0 && !self.positive {
+		self.hue_distance -= 360.0
+	}
+}
+func (self *ModeGradientPosition) Randomize() {
+	self.percent = rand.Float64() * 100.0
+	self.hue_from = rand.Float64() * 360.0
+	self.Randomize_wo_from()
+}
+
 func (self *ModeGradient) postSetParameter() {
-	self.progressDegStepSize = 360 / (float64(self.parameter.RoundTimeMs) / 1000) * (float64(RefreshIntervalNs) / 1000 / 1000 / 1000)
+	self.positions = make([]ModeGradientPosition, self.parameter.Count)
+	for i := range self.positions {
+		self.positions[i].percent = 0
+		self.positions[i].limits = &self.limits
+		self.positions[i].Randomize()
+	}
 	for i := 0; i < len(self.ledsHSV); i++ {
 		self.ledsHSV[i].V = self.parameter.Brightness
 	}
 }
 
-func (self *ModeGradient) shiftGradiant() {
-	for i := 2; i > 0; i-- {
-		self.hues[i] = self.hues[i-1]
-		self.clockwise[i] = self.clockwise[i-1]
+func (self *ModeGradient) calcDisplayWoStep() {
+	for i := 0; i < len(self.ledsHSV); i++ {
+		relPos := float64(i) / float64(len(self.ledsHSV)) * float64(self.parameter.Count-1) //eg count 2 -> 1.0, count=3 ->2
+		huePos := int(relPos)
+		self.ledsHSV[i].H = self.positions[huePos].hue_current
 	}
-	self.hues[0] = rand.Float64() * 360.0
-	self.clockwise[0] = rand.Int()%2 == 1
+	// self.percent += self.percentStep
+	// if self.percent >= 360.0 {
+	// 	self.shiftGradiant()
+	// 	self.percent -= 360.0
+	// }
+
+	// for i := 0; i < len(self.ledsHSV); i++ {
+	// 	SCALE := 1.0
+	// 	pos := self.percent/360.0 - SCALE*float64(i)/float64(len(self.ledsHSV)-1)
+	// 	hueI := int(pos)
+	// 	if hueI < 0 {
+	// 		hueI *= -1
+	// 	}
+	// 	posRel := pos + float64(hueI)
+	// 	hue := (self.hues[hueI]*(posRel) + self.hues[hueI+1]*(1.0-posRel))
+	// 	if hue > 360.0 {
+	// 		hue -= 360.0
+	// 	} else if hue < 0 {
+	// 		hue += 360.0
+	// 	}
+	// 	self.ledsHSV[i].H = hue
+	// }
+
+	self.display.ApplySingleRowHSV(self.ledsHSV)
 }
 
 func (self *ModeGradient) calcDisplay() {
-	self.progressDegStep += self.progressDegStepSize
-	if self.progressDegStep >= 360.0 {
-		self.shiftGradiant()
-		self.progressDegStep -= 360.0
+	for i := range self.positions {
+		self.positions[i].StepForward()
 	}
-
-	for i := 0; i < len(self.ledsHSV); i++ {
-		SCALE := 1.0
-		pos := self.progressDegStep/360.0 - SCALE*float64(i)/float64(len(self.ledsHSV)-1)
-		hueI := int(pos)
-		if hueI < 0 {
-			hueI *= -1
-		}
-		posRel := pos + float64(hueI)
-		hue := (self.hues[hueI]*(posRel) + self.hues[hueI+1]*(1.0-posRel))
-		if hue > 360.0 {
-			hue -= 360.0
-		} else if hue < 0 {
-			hue += 360.0
-		}
-		self.ledsHSV[i].H = hue
-	}
-
-	self.display.ApplySingleRowHSV(self.ledsHSV)
+	self.calcDisplayWoStep()
 }
 
 func (self *ModeGradient) Randomize() {
 	rand.Seed(time.Now().UnixNano())
 	self.setParameter(ModeGradientParameter{
-		RoundTimeMs: uint32(rand.Float32()*float32(self.limits.MaxRoundTimeMs-self.limits.MinRoundTimeMs)) + self.limits.MinRoundTimeMs,
-		Brightness:  rand.Float64()*(self.limits.MaxBrightness-self.limits.MinBrightness) + self.limits.MinBrightness,
-		Reverse:     rand.Int()%2 == 1,
+		Brightness: rand.Float64()*(self.limits.MaxBrightness-self.limits.MinBrightness) + self.limits.MinBrightness,
+		Count:      rand.Uint32()%(self.limits.MaxCount-self.limits.MinCount) + self.limits.MinCount,
 	})
 }
--- mode/gradient_test.go
+++ mode/gradient_test.go
@@ -8,49 +8,65 @@ import (
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
-func TestHelloName(t *testing.T) {
+func TestModeGradient(t *testing.T) {
 	ledCount := 5
 	dbdriver, _ := dbdriver.NewDbDriver("../tst/temp/db")
 	display := display.NewDisplay(ledCount, 1, "0", "0")
-	roundTimeMs := uint32(1000)
-	// progressDegStepSize := 360 / (float64(roundTimeMs) / 1000) * (float64(RefreshIntervalNs) / 1000 / 1000 / 1000)
+	// roundTimeMs := uint32(1000)
+	// // progressDegStepSize := 360 / (float64(roundTimeMs) / 1000) * (float64(RefreshIntervalNs) / 1000 / 1000 / 1000)
 
 	modeGradient := NewModeGradient(dbdriver, display)
-	modeGradientParameter := ModeGradientParameter{Brightness: 1.0, RoundTimeMs: roundTimeMs, Reverse: false}
+	modeGradientParameter := ModeGradientParameter{Brightness: 1.0, Count: 3}
 	modeGradient.setParameter(modeGradientParameter)
-	modeGradient.progressDegStep = 360.0 - modeGradient.progressDegStepSize
-
-	modeGradient.hues[0] = 80
-	modeGradient.hues[1] = 120
-
-	hues := modeGradient.hues //copy by value
-
+	for i := range modeGradient.positions {
+		modeGradient.positions[i].percent = 0
+	}
+	if modeGradient.positions[0].percent != 0 {
+		t.Fatalf(`progress set not working`)
+	}
 	modeGradient.calcDisplay()
-	modeGradient.hues[0] = 40
+	if modeGradient.positions[0].percent == 0 {
+		t.Fatalf(`progress not increasing`)
+	}
+	if modeGradient.positions[0].hue_current != modeGradient.positions[0].hue_from {
+		t.Fatalf(`first position is wrong`)
+	}
+	if modeGradient.ledsHSV[0].H != modeGradient.positions[0].hue_current {
+		t.Fatalf(`first color is wrong`)
+	}
+	// modeGradient.progressDegStep = 360.0 - modeGradient.progressDegStepSize
 
-	if modeGradient.progressDegStep != 0 {
-		t.Fatalf(`progressDegStep is false`)
-	}
-	if modeGradient.hues[1] != hues[0] {
-		t.Fatalf(`led shift not working`)
-	}
-	if modeGradient.ledsHSV[0].H != 80 {
-		t.Fatalf(`first led has wrong color`)
-	}
+	// modeGradient.hues[0] = 80
+	// modeGradient.hues[1] = 120
 
-	if modeGradient.ledsHSV[ledCount-1].H != 120 {
-		t.Fatalf(`last led wrong`)
-	}
+	// hues := modeGradient.hues //copy by value
 
-	for i := 0; i < FPS; i++ {
-		modeGradient.calcDisplay()
-	}
-	if modeGradient.ledsHSV[0].H != 40 {
-		t.Fatalf(`first led has wrong color`)
-	}
+	// modeGradient.calcDisplay()
+	// modeGradient.hues[0] = 40
 
-	if modeGradient.ledsHSV[ledCount-1].H != 80 {
-		t.Fatalf(`last led wrong`)
-	}
+	// if modeGradient.progressDegStep != 0 {
+	// 	t.Fatalf(`progressDegStep is false`)
+	// }
+	// if modeGradient.hues[1] != hues[0] {
+	// 	t.Fatalf(`led shift not working`)
+	// }
+	// if modeGradient.ledsHSV[0].H != 80 {
+	// 	t.Fatalf(`first led has wrong color`)
+	// }
+
+	// if modeGradient.ledsHSV[ledCount-1].H != 120 {
+	// 	t.Fatalf(`last led wrong`)
+	// }
+
+	// for i := 0; i < FPS; i++ {
+	// 	modeGradient.calcDisplay()
+	// }
+	// if modeGradient.ledsHSV[0].H != 40 {
+	// 	t.Fatalf(`first led has wrong color`)
+	// }
+
+	// if modeGradient.ledsHSV[ledCount-1].H != 80 {
+	// 	t.Fatalf(`last led wrong`)
+	// }
 
 }
