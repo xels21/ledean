f0f030ea52a06f360a15f2ff625088ac6aae03ef
xels21 <dean.poslovsky@gmail.com>
2023-02-22 19:36:51 +0100

    esp32 babstadt

--- main_tinygo.go
+++ main_tinygo.go
@@ -14,7 +14,7 @@ func main() {
 	//ESP32 REICHI
 	// os.Args = []string{"tinygo_stub", "-gpio_led_data=23", "-gpio_button=22", "-led_count=50", "-direct_start"}
 	//ESP32 BABSTADT
-	// os.Args = []string{"tinygo_stub", "-gpio_led_data=23", "-gpio_button=22", "-led_count=28", "-direct_start"}
+	os.Args = []string{"tinygo_stub", "-gpio_led_data=23", "-gpio_button=22", "-led_count=50", "-direct_start"}
 	//Arduino Nano
 	// os.Args = []string{"tinygo_stub", "-gpio_led_data=2", "-gpio_button=22", "-led_count=50", "-direct_start"}
 	parm := ledean.GetParameter()
