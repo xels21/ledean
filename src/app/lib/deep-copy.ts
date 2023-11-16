b85c1b3edc76d5937b9fce564229a91c9c84512c
Xels <00deansen@gmx.de>
2023-11-16 17:28:26 +0100

    angular update 14->15

--- src/app/lib/deep-copy.ts
+++ src/app/lib/deep-copy.ts
@@ -17,7 +17,7 @@ export const deepCopy = <T>(target: T): T => {
     (target as any[]).forEach((v) => { cp.push(v); });
     return cp.map((n: any) => deepCopy<any>(n)) as any;
   }
-  if (typeof target === 'object' && target !== {}) {
+  if (typeof target === 'object' ) {//&& target !== {}) {
     const cp = { ...(target as { [key: string]: any }) } as { [key: string]: any };
     Object.keys(cp).forEach(k => {
       cp[k] = deepCopy<any>(cp[k]);
