b2fb1f85b30f98f7cd6f84912751cb030a194bbb
Xels <00deansen@gmx.de>
2023-12-02 15:23:31 +0100

    added spectrum finished

--- mode/spectrum.go
+++ mode/spectrum.go
@@ -11,12 +11,35 @@ import (
 )
 
 type ModeSpectrumPosition struct {
-	// hueFrom720    float64
-	// hueTo720      float64
-	// hueCurrent720 float64
-	// hueDistance   float64
-	// pPercentStep  *float64
-	// percent       float64
+	factor            float64
+	factorPercent     float64
+	factorPercentStep float64
+	offset            float64
+	offsetPercent     float64
+	offsetPercentStep float64
+	parm              *ModeSpectrumParameterPosition
+}
+
+func (self *ModeSpectrumPosition) StepForward() {
+	self.factorPercent += self.factorPercentStep
+	for self.factorPercent >= 1.0 {
+		self.factorPercent -= 1.0
+	}
+	self.factor = ((self.parm.FacTo - self.parm.FacFrom) * (math.Sin(self.factorPercent*2*math.Pi)*.5 + .5)) + self.parm.FacFrom
+
+	self.offsetPercent += self.offsetPercentStep
+	for self.offsetPercent >= 1.0 {
+		self.offsetPercent -= 1.0
+	}
+	self.offset = ((self.parm.OffTo - self.parm.OffFrom) * (math.Sin(self.offsetPercent*2*math.Pi)*.5 + .5)) + self.parm.OffFrom
+
+}
+
+func (self *ModeSpectrumPosition) postSetParameter() {
+	self.factorPercentStep = 1.0 / float64(self.parm.FacRoundTimeMs) * float64(RefreshIntervalNs) / 1000.0 /*us*/ / 1000.0 /*ms*/
+	self.offsetPercentStep = 1.0 / float64(self.parm.OffRoundTimeMs) * float64(RefreshIntervalNs) / 1000.0 /*us*/ / 1000.0 /*ms*/
+	self.factorPercent = rand.Float64()
+	self.offsetPercent = rand.Float64()
 }
 
 type ModeSpectrum struct {
@@ -24,46 +47,50 @@ type ModeSpectrum struct {
 	parameter ModeSpectrumParameter
 	limits    ModeSpectrumLimits
 	ledsHSV   []color.HSV
-	// percentStep  float64
-	// positions    []ModeSpectrumPosition
-	// posDistances []float64
+	positions [2]ModeSpectrumPosition
+}
+
+type ModeSpectrumParameterPosition struct {
+	FacFrom        float64 `json:"facFrom"`
+	FacTo          float64 `json:"facTo"`
+	FacRoundTimeMs uint32  `json:"facRoundTimeMs"`
+	OffFrom        float64 `json:"offFrom"`
+	OffTo          float64 `json:"offTo"`
+	OffRoundTimeMs uint32  `json:"offRoundTimeMs"`
 }
 
 type ModeSpectrumParameter struct {
-	HueFrom720 float64 `json:"hueFrom720"`
-	HueTo720   float64 `json:"hueTo720"`
-	Brightness float64 `json:"brightness"`
-	// Count       uint32  `json:"count"`
-	// RoundTimeMs uint32  `json:"roundTimeMs"`
+	HueFrom720 float64                          `json:"hueFrom720"`
+	HueTo720   float64                          `json:"hueTo720"`
+	Brightness float64                          `json:"brightness"`
+	Positions  [2]ModeSpectrumParameterPosition `json:"positions"`
 }
 type ModeSpectrumLimits struct {
-	// MinRoundTimeMs uint32  `json:"minRoundTimeMs"`
-	// MaxRoundTimeMs uint32  `json:"maxRoundTimeMs"`
-	MinBrightness float64 `json:"minBrightness"`
-	MaxBrightness float64 `json:"maxBrightness"`
-	// MinCount       uint32  `json:"minCount"`
-	// MaxCount       uint32  `json:"maxCount"`
+	MaxRoundTimeMs uint32  `json:"maxRoundTimeMs"`
+	MinRoundTimeMs uint32  `json:"minRoundTimeMs"`
+	MinBrightness  float64 `json:"minBrightness"`
+	MaxBrightness  float64 `json:"maxBrightness"`
+	MinFactor      float64 `json:"minFactor"`
+	MaxFactor      float64 `json:"maxFactor"`
+	MinOffset      float64 `json:"minOffset"`
+	MaxOffset      float64 `json:"maxOffset"`
 }
 
 func NewModeSpectrum(dbdriver *dbdriver.DbDriver, display *display.Display) *ModeSpectrum {
 	self := ModeSpectrum{
 		limits: ModeSpectrumLimits{
-			// MinRoundTimeMs: 1000,
-			// MaxRoundTimeMs: 10000,
-			MinBrightness: 0.01,
-			MaxBrightness: 1.0,
-			// MinCount:       2,
-			// MaxCount:       6,
+			MinBrightness:  0.01,
+			MaxBrightness:  1.0,
+			MinRoundTimeMs: 10000.0,
+			MaxRoundTimeMs: 60000.0,
+			MinFactor:      0.0,
+			MaxFactor:      10.0,
+			MinOffset:      0.0,
+			MaxOffset:      math.Pi,
 		},
 	}
 
 	self.ModeSuper = *NewModeSuper(dbdriver, display, "ModeSpectrum", RenderTypeDynamic, self.calcDisplay)
-
-	// self.posDistances = make([]float64, self.limits.MaxCount-1)
-	// self.positions = make([]ModeSpectrumPosition, self.limits.MaxCount)
-	// for i := range self.positions {
-	// self.positions[i].pPercentStep = &self.percentStep
-	// }
 
 	self.ledsHSV = make([]color.HSV, self.display.GetRowLedCount())
 	for i := 0; i < len(self.ledsHSV); i++ {
@@ -95,96 +122,77 @@ func (self *ModeSpectrum) TrySetParameter(b []byte) error {
 	return nil
 }
 
-func (self *ModeSpectrum) SetParameter(parm ModeSpectrumParameter) {
-	if parm.HueFrom720 > parm.HueTo720 {
-		hueTo720 := parm.HueFrom720
-		parm.HueFrom720 = parm.HueTo720
-		parm.HueTo720 = hueTo720
+func (self *ModeSpectrum) correctParameter() {
+	if self.parameter.HueFrom720 > self.parameter.HueTo720 {
+		hueTo720 := self.parameter.HueFrom720
+		self.parameter.HueFrom720 = self.parameter.HueTo720
+		self.parameter.HueTo720 = hueTo720
 	}
+	for i := range self.parameter.Positions {
+		if self.parameter.Positions[i].FacFrom > self.parameter.Positions[i].FacTo {
+			facTo := self.parameter.Positions[i].FacFrom
+			self.parameter.Positions[i].FacFrom = self.parameter.Positions[i].FacTo
+			self.parameter.Positions[i].FacTo = facTo
+		}
+		if self.parameter.Positions[i].OffFrom > self.parameter.Positions[i].OffTo {
+			offTo := self.parameter.Positions[i].OffFrom
+			self.parameter.Positions[i].OffFrom = self.parameter.Positions[i].OffTo
+			self.parameter.Positions[i].OffTo = offTo
+		}
+	}
+}
+
+func (self *ModeSpectrum) SetParameter(parm ModeSpectrumParameter) {
 	self.parameter = parm
+	self.correctParameter()
 	self.dbdriver.Write(self.GetName(), "parameter", self.parameter)
 	self.postSetParameter()
 }
 
-// func (self *ModeSpectrumPosition) StepForward() {
-// 	self.hueCurrent720 = self.hueFrom720 + self.hueDistance*self.percent/100
-
-// 	self.percent += *self.pPercentStep
-// 	if self.percent > 100 {
-// 		self.percent -= 100
-// 		self.hueFrom720 = self.hueTo720
-// 		self.randomizeWoFrom()
-// 	}
-// }
-// func (self *ModeSpectrumPosition) randomizeWoFrom() {
-// 	self.hueTo720 = rand.Float64() * 720.0
-
-// 	self.hueDistance = self.hueTo720 - self.hueFrom720
-// }
-
-// func (self *ModeSpectrumPosition) Randomize() {
-// self.percent = rand.Float64() * 100.0
-// self.hueFrom720 = rand.Float64() * 720.0
-// self.randomizeWoFrom()
-// }
-
 func (self *ModeSpectrum) postSetParameter() {
-	// self.percentStep = 100 / (float64(self.parameter.RoundTimeMs) / 1000) * (float64(RefreshIntervalNs) / 1000 / 1000 / 1000)
-	//
-	// for i := range self.positions {
-	// self.positions[i].Randomize()
-	// }
+	for i := range self.positions {
+		self.positions[i].parm = &self.parameter.Positions[i]
+		self.positions[i].postSetParameter()
+	}
 
 	for i := 0; i < len(self.ledsHSV); i++ {
 		self.ledsHSV[i].V = self.parameter.Brightness
 	}
 }
 
-// func (self *ModeSpectrum) calcDisplayWoStep() {
-// for i := 0; i < len(self.ledsHSV); i++ {
-// absPos := float64(i) / float64(len(self.ledsHSV)) * float64(self.parameter.Count-1) //eg count 2 -> 1.0, count=3 ->2
-// huePos := int(absPos)
-//
-// relPos := absPos - float64(huePos)
-//
-// self.ledsHSV[i].H = self.positions[huePos].hueCurrent720 + self.posDistances[huePos]*relPos
-// }
-
-// self.display.ApplySingleRowHSV(self.ledsHSV)
-// }
-
 func (self *ModeSpectrum) calcDisplay() {
-	// for i := 0; i < int(self.parameter.Count); i++ {
-	// self.positions[i].StepForward()
-	// }
-	// for i := 0; i < int(self.parameter.Count-1); i++ {
-	// self.posDistances[i] = self.positions[1+i].hueCurrent720 - self.positions[i].hueCurrent720
-	// }
-	// self.calcDisplayWoStep()
+	for i := range self.positions {
+		self.positions[i].StepForward()
+	}
 
 	hueDist := self.parameter.HueTo720 - self.parameter.HueFrom720
 
 	for i := 0; i < len(self.ledsHSV); i++ {
 		x := (float64(i) / float64(len(self.ledsHSV))) * 2 * math.Pi
-		self.ledsHSV[i].H = self.parameter.HueFrom720 + ((math.Sin(2*x+2)*math.Sin(.5*x+4)*0.5)+.5)*hueDist
-		// absPos := x * float64(self.parameter.Count-1) //eg count 2 -> 1.0, count=3 ->2
-		// huePos := int(absPos)
-
-		// relPos := absPos - float64(huePos)
-
-		// self.ledsHSV[i].H = self.positions[huePos].hueCurrent720 + self.posDistances[huePos]*relPos
+		self.ledsHSV[i].H = self.parameter.HueFrom720 + ((math.Sin(self.positions[0].factor*x+self.positions[0].offset)*math.Cos(self.positions[1].factor*x+self.positions[1].offset)*0.5)+.5)*hueDist
 	}
 
 	self.display.ApplySingleRowHSV(self.ledsHSV)
 }
 
+func (self *ModeSpectrum) getRandomPosition(seed int64) ModeSpectrumParameterPosition {
+	r := rand.New(rand.NewSource(seed + time.Now().UnixNano()))
+	return ModeSpectrumParameterPosition{
+		FacFrom:        r.Float64()*(self.limits.MaxFactor-self.limits.MinFactor) + self.limits.MinFactor,
+		FacTo:          r.Float64()*(self.limits.MaxFactor-self.limits.MinFactor) + self.limits.MinFactor,
+		FacRoundTimeMs: uint32(r.Float64()*(float64(self.limits.MaxRoundTimeMs)-float64(self.limits.MinRoundTimeMs)) + float64(self.limits.MinRoundTimeMs)),
+		OffFrom:        r.Float64()*(self.limits.MaxOffset-self.limits.MinOffset) + self.limits.MinOffset,
+		OffTo:          r.Float64()*(self.limits.MaxOffset-self.limits.MinOffset) + self.limits.MinOffset,
+		OffRoundTimeMs: uint32(r.Float64()*(float64(self.limits.MaxRoundTimeMs)-float64(self.limits.MinRoundTimeMs)) + float64(self.limits.MinRoundTimeMs)),
+	}
+}
+
 func (self *ModeSpectrum) Randomize() {
-	rand.Seed(time.Now().UnixNano())
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	self.SetParameter(ModeSpectrumParameter{
-		Brightness: rand.Float64()*(self.limits.MaxBrightness-self.limits.MinBrightness) + self.limits.MinBrightness,
-		HueFrom720: rand.Float64() * 720.0,
-		HueTo720:   rand.Float64() * 720.0,
-		// Count:       rand.Uint32()%(self.limits.MaxCount-self.limits.MinCount) + self.limits.MinCount,
-		// RoundTimeMs: uint32(rand.Float32()*float32(self.limits.MaxRoundTimeMs-self.limits.MinRoundTimeMs)) + self.limits.MinRoundTimeMs,
+		Brightness: r.Float64()*(self.limits.MaxBrightness-self.limits.MinBrightness) + self.limits.MinBrightness,
+		HueFrom720: r.Float64() * 720.0,
+		HueTo720:   r.Float64() * 720.0,
+		Positions:  [2]ModeSpectrumParameterPosition{self.getRandomPosition(0), self.getRandomPosition(1)},
 	})
 }
