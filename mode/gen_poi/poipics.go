6e6a540006c8771dab03a8c78efa4e97bf0e90ae
Xels <00deansen@gmx.de>
2024-08-16 17:17:00 +0200

    new pics (not working)

--- mode/gen_poi/poipics.go
+++ mode/gen_poi/poipics.go
@@ -4,4 +4,4 @@ import "image"
 
 var PixelCount = 50
 
-var PoiPics = []*image.NRGBA{&PoiPic_fire, &PoiPic_wave, &PoiPic_man_flower, &PoiPic_man_flower_2, &PoiPic_mario, &PoiPic_weed} //&PoiPic_psych,
+var PoiPics = []*image.NRGBA{&PoiPic_fire, &PoiPic_geo, &PoiPic_honeycorb, &PoiPic_man_flower, &PoiPic_man_flower_2, &PoiPic_mario, &PoiPic_mosaik, &PoiPic_nyan, &PoiPic_rainbow, &PoiPic_roe, &PoiPic_simple_cyan, &PoiPic_star, &PoiPic_tiles_top, &PoiPic_wave, &PoiPic_weed, }
